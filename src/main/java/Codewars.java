d3ade5d86ae19fe6c777f064a3c8ef5e8b62ceda
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-02-15 20:01:54 -0500

    maskify exercise

--- src/main/java/Codewars.java
+++ src/main/java/Codewars.java
@@ -1,7 +1,14 @@
 public class Codewars {
 
     public static String maskify(String str) {
-        throw new UnsupportedOperationException("Unimplemented");
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.length() > 4 && i < str.length() - 4) {
+                result += "#";
+            } else {
+                result += str.charAt(i);
+            }
+        }
+        return result;
     }
-
 }
