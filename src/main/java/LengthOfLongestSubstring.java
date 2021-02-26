fbca3e455f2237e14ec15df8df0e0df168e869c3
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-02-26 12:36:55 -0500

    Length of Longest Substring

---
+++ src/main/java/LengthOfLongestSubstring.java
@@ -0,0 +1,16 @@
+// Given a string s, find the length of the longest substring without
+// repeating characters.
+
+public class LengthOfLongestSubstring {
+
+    public int lengthOfLongestSubstring(String s) {
+        int counter = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 1; j < s.length(); j++) {
+                if (s.charAt(i) != s.charAt(j)) {
+                    counter++;
+                }
+            }
+        } return counter;
+    }
+}
