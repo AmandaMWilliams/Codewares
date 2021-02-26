5a1d3c9957581989ca2f2b0d2e0403c5107a84cf
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-02-26 13:51:59 -0500

    Even or Odd problem added

--- src/main/java/EvenOrOdd.java
+++ src/main/java/EvenOrOdd.java
@@ -1,2 +1,8 @@
+//Create a function (or write a script in Shell) that takes an integer
+// as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
 public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+
+    }
 }
