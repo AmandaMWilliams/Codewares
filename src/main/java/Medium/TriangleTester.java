8a7934f31ed39515ab6fd6ec7a20cc86deffce04
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-02-27 11:18:38 -0500

    Trangle Tester

---
+++ src/main/java/Medium/TriangleTester.java
@@ -0,0 +1,11 @@
+package Medium;
+
+public class TriangleTester {
+    public static boolean isTriangle(int a, int b, int c) {
+        if (((a+b) > c) && ((b+c) > a) && ((a+c) > b)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
