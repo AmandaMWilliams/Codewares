c45acee5897ff2a4bbf0ba1d4b1b1c6d45df5a18
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-02-27 10:55:35 -0500

    Added Grasshopper

---
+++ src/main/java/Grasshopper.java
@@ -0,0 +1,12 @@
+// Write a program that finds the summation of every number from 1 to num.
+// The number will always be a positive integer greater than 0.
+
+public class Grasshopper {
+    public static int summation(int n) {
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/GrasshopperTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GrasshopperTest {
+    @Test
+    public void test1() {
+        assertEquals(1,
+                Grasshopper.summation(1));
+    }
+    @Test
+    public void test2() {
+        assertEquals(36,
+                Grasshopper.summation(8));
+    }
+}
