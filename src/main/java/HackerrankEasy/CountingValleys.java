87e2a2b4491fffbf349efe4a6ba25ad77675296d
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-03-08 12:35:08 -0500

    CountingValleys

--- src/main/java/HackerrankEasy/CountingValleys.java
+++ src/main/java/HackerrankEasy/CountingValleys.java
@@ -1,4 +1,65 @@
 package HackerrankEasy;
 
+import java.io.*;
+
+//Given the sequence of up and down steps during a hike, find and print
+// the number of valleys walked through. A valley is a sequence of
+// consecutive steps below sea level, starting with a step down from sea
+// level and ending with a step up to sea level.
+
 public class CountingValleys {
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, String path) {
+        int numberOfValleys = 0;
+        int elevation = 0;
+
+        if (steps > 0) {
+            for (int i = 0; i < steps; i++) {
+                if (elevation == 0 && path.charAt(i) == 'D'){
+                    elevation--;
+                    numberOfValleys++;
+                } else if (elevation == 0 && path.charAt(i) == 'U'){
+                    elevation++;
+                } else if (elevation < 0 && path.charAt(i) == 'D'){
+                    elevation--;
+                } else if (elevation < 0 && path.charAt(i) == 'U'){
+                    elevation++;
+                } else if (elevation > 0 && path.charAt(i) == 'D'){
+                    elevation--;
+                } else {
+                    elevation++;
+                }
+            }
+        }
+        return numberOfValleys;
+    }
+
+
+    public static class Solution {
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+            int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+            String path = bufferedReader.readLine();
+
+            int result = CountingValleys.countingValleys(steps, path);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+
+            bufferedReader.close();
+            bufferedWriter.close();
+        }
+    }
+
 }
--- src/test/java/HackerRankTests/CountingValleysTest.java
+++ src/test/java/HackerRankTests/CountingValleysTest.java
@@ -1,4 +1,33 @@
 package HackerRankTests;
 
+import HackerrankEasy.CountingValleys;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CountingValleysTest {
+    CountingValleys c = new CountingValleys();
+
+    @Test
+    public void countingValleysTest(){
+        //given
+        int testSteps = 8;
+        String testPath = "UDDDUDUU";
+        int expected = 1;
+        //when
+        int actual = c.countingValleys(testSteps, testPath);
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countingValleysTest2(){
+        //given
+        int testSteps = 12;
+        String testPath = "DDUUDDUDUUUD";
+        int expected = 2;
+        //when
+        int actual = c.countingValleys(testSteps, testPath);
+        //then
+        Assert.assertEquals(expected, actual);
+    }
 }
