409230f34518fe8a18b7815d22540c15e63b1851
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-03-22 12:30:10 -0400

    Working on LongestConsec

--- src/main/java/Level6/LongestConsecString.java
+++ src/main/java/Level6/LongestConsecString.java
@@ -6,7 +6,18 @@ package Level6;
 
 public class LongestConsecString {
     public static String longestConsec(String[] strarr, int k) {
-        // your code
-        return null;
+        String longestConsecutiveString = "";
+        int longestLength = 0;
+        for (int i = 0; i < strarr.length; i++) {
+            String currentString = "";
+            for (int j = i; j < i+k; j++) {
+                currentString += strarr[j];
+            }
+            if (currentString.length() > longestLength) {
+                longestConsecutiveString = currentString;
+                longestLength = longestConsecutiveString.length();
+            }
+        }
+        return longestConsecutiveString;
     }
 }
