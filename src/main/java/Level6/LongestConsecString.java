33818ce433de3025d3b4f287197483a9ca25dc9a
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-03-22 11:41:46 -0400

    Added longestConsec

---
+++ src/main/java/Level6/LongestConsecString.java
@@ -0,0 +1,12 @@
+package Level6;
+
+// Given an array(list) strarr of strings and an integer k, return the
+// first longest string consisting of k consecutive strings taken in the
+// array.
+
+public class LongestConsecString {
+    public static String longestConsec(String[] strarr, int k) {
+        // your code
+        return null;
+    }
+}
---
+++ src/test/java/CodeWarsTests/LongestConsecTest.java
@@ -0,0 +1,26 @@
+package CodeWarsTests;
+import static org.junit.Assert.*;
+
+import Level6.LongestConsecString;
+import org.junit.Test;
+
+public class LongestConsecTest {
+
+    private static void testing(String actual, String expected) {
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test() {
+        System.out.println("longestConsec Fixed Tests");
+        testing(LongestConsecString.longestConsec(new String[] {"zone",
+                "abigail", "theta", "form", "libe", "zas", "theta", "abigail"}, 2), "abigailtheta");
+        testing(LongestConsecString.longestConsec(new String[] {"ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"}, 1), "oocccffuucccjjjkkkjyyyeehh");
+        testing(LongestConsecString.longestConsec(new String[] {}, 3), "");
+        testing(LongestConsecString.longestConsec(new String[] {"itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"}, 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
+        testing(LongestConsecString.longestConsec(new String[] {"wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"}, 2), "wlwsasphmxxowiaxujylentrklctozmymu");
+        testing(LongestConsecString.longestConsec(new String[] {"zone", "abigail", "theta", "form", "libe", "zas"}, -2), "");
+        testing(LongestConsecString.longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 3), "ixoyx3452zzzzzzzzzzzz");
+        testing(LongestConsecString.longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 15), "");
+        testing(LongestConsecString.longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 0), "");
+    }
+}
